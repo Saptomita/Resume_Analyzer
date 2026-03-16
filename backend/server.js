e8233db5f3d8df4cf7a68cf1c6a03f893029fa13
Saptomita <saptomitaghosh2005@gmail.com>
2026-03-16 21:13:11 +0530

    added server start port

--- backend/server.js
+++ backend/server.js
@@ -125,4 +125,10 @@ app.post("/analyze", upload.single("resume"), async (req, res) => {
 
 });
 
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 
